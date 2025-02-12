e2422f576bc5558e374b0c8a0b3aea850c3d7732
Mdkaif2782 <mibnzaman02@gmail.com>
2025-02-12 11:18:17 +0600

    cors error solve

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app/app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 const main = async () => {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  });
+  const config = new DocumentBuilder()
+    .setTitle("Random Project")
+    .setDescription("Api for random project")
+    .setVersion("1.0")
+    .addTag("random")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
   await app.listen(1337);
 };
 
