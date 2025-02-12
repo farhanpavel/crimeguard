e981e1eb8ce74ca0d1db1219ed2f3ad88e9da7dc
Mdkaif2782 <mibnzaman02@gmail.com>
2025-02-12 10:48:34 +0600

    fixed directory errors

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "../auth/auth.module";
+import { MediaModule } from "../media/media/media.module";
 
 @Module({
   imports: [
@@ -10,7 +11,8 @@ import { AuthModule } from "../auth/auth.module";
       isGlobal: true,
       cache: true
     }),
-    AuthModule
+    AuthModule,
+    MediaModule
   ],
   controllers: [AppController],
   providers: [AppService]
